25ef5a6cdfbb9658ae2f97becc9cc4d8201d1cc6
Harytan <92979595+Yarnyan@users.noreply.github.com>
2024-10-30 01:51:36 +0500

    update

--- src/components/tarrif/Tarrif.tsx
+++ src/components/tarrif/Tarrif.tsx
@@ -1,30 +1,37 @@
 import { splitFormatText } from '../../helpers/split/split';
 import { FaCheck } from "react-icons/fa";
+import { useBuySubsMutation } from '../../module/tarrifs/api/subs';
 type Props = {
     id: number;
     name: string;
     price: number;
     description: string;
-    pros: string;
+    // pros: string;
 };
 
-export default function Tarrif({ id, name, price, description, pros }: Props) {
+export default function Tarrif({ id, name, price, description }: Props) {
+    const [buySubscribe] = useBuySubsMutation()
+    const buySubs = (id: number) => {
+        const formData = new FormData()
+        formData.append('subId', id.toString())
+        buySubscribe(formData)
+    }
     return (
-        <div className='mt-4 tarrif-container flex flex-col justify-between px-4 pt-6 bg-[var(--sideBarColor)] rounded-2xl p-6 max-w-[300px]'>
+        <div className='mt-4 tarrif-container flex flex-col justify-between px-4 pt-6 bg-[var(--sideBarColor)] rounded-2xl p-6 w-full max-w-[300px] sm:min-w-[90%]'>
             <div>
                 <h1 className='text-3xl font-normal text-[var(--textColor)] text-center'>{name}</h1>
-                <p className='mt-4 text-l text-[var(--mutedTextColor)] text-center'>{description}</p>
+                <p className='mt-4 text-l text-[var(--mutedTextColor)] text-center'>Subscriptions are suitable for both companies and users</p>
                 <div className='mt-4 flex items-center justify-center flex-col'>
                     <p className='text-2xl font-normal text-[var(--textColor)] text-center'>{price} €</p>
                     <p className='text-l font-normal text-[var(--mutedTextColor)] text-center'>per month</p>
                 </div>
                 <div className='mt-4 flex items-center justify-center flex-col'>
-                    <button className='min-w-[120px] text-m font-normal bg-[#F5F5F5] h-[40px] rounded-xl text-[#000000] hover:bg-[#d3d3d6] duration-300'>Get started</button>
+                    <button onClick={() => buySubs(id)} className='min-w-[120px] text-m font-normal bg-[#F5F5F5] h-[40px] rounded-xl text-[#000000] hover:bg-[#d3d3d6] duration-300'>Get started</button>
                 </div>
                 <div className='mt-4 flex items-center justify-center flex-col'>
                     <p className='text-l text-[var(--textColor)]'>Everything in {name}, plus:</p>
                     <div className='mt-4'>
-                        {splitFormatText(pros).map((item) => {
+                        {splitFormatText(description).map((item) => {
                             return (
                                 <div className='flex items-center'>
                                     <FaCheck fill='#a2a2ac' size={14}/>
--- src/module/tarrifs/Tarrifs.tsx
+++ src/module/tarrifs/Tarrifs.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { isApiError } from '../../helpers/auth/apiError'
 import Tarrif from '../../components/tarrif/Tarrif'
 import { ITarrif } from './entity/entity'
+import { useLazyGetSubsQuery } from './api/subs'
 type Props = {}
 
 export default function Tarrifs({ }: Props) {
+  const [getSubs, subs] = useLazyGetSubsQuery()
   const data: ITarrif[] = [
     {
       id: 1,
@@ -34,12 +37,23 @@ export default function Tarrifs({ }: Props) {
       pros: "5 Users included; Up to 30 user seats; 1 Space included; Activity Log; Custom Roles; Replace Assets; Translatable Slugs; Webhook Secrets; 500GB Traffic/month included; 5 Users included; Up to 30 user seats; 1 Space included; Activity Log; Custom Roles; Replace Assets; Translatable Slugs; Webhook Secrets; 500GB Traffic/month included",
     },
   ]
+
+  useEffect(() => {
+    try {
+      getSubs(null)
+    } catch (error) {
+
+    }
+    return () => {
+
+    };
+  }, []);
   return (
     <div className="w-full sm:justify-center">
       <div className='flex mt-4 align-center w-full flex-wrap sm:items-center sm:justify-center'>
-        {data.map((tarrif) => {
+        {subs.data?.map((tarrif: any) => {
           return (
-            <Tarrif id={tarrif.id} name={tarrif.name} price={tarrif.price} description={tarrif.description} pros={tarrif.pros} key={tarrif.id} />
+            <Tarrif id={tarrif.id} name={tarrif.name} price={tarrif.price} description={tarrif.description} key={tarrif.id} />
           )
         })}
       </div>
